8b6599810c77e581caab7e471a7da3e82f25f21c
Ricardo Boraure <r.boraure@kation.com.do>
2022-06-04 15:33:49 -0400

    Add product type and env variable

--- types/type.d.ts
+++ types/type.d.ts
@@ -1,6 +1,36 @@
+import { type } from 'os';
+
 type FirebaseError = {
   type: string,
   message: unknown
+}
+
+type Img = {
+  src: string;
+  alt: string;
+}
+
+type Dimentions = {
+  height: number;
+  width: number
+}
+
+interface Product {
+  name: string;
+  price: number;
+  currency: string;
+  bestseller: boolean;
+  featured: boolean;
+  image: Img;
+}
+
+interface FeaturedProduct extends Product {
+  details: {
+    recomendations: Img[];
+    description: string;
+    size: number;
+    dimentions: Dimentions;
+  }
 }
 
 type IconProps = import('./icons').IconProps
