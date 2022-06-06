d23484b0834c1d94a99736e8f86eeb0c57f65a72
Ricardo Boraure <r.boraure@kation.com.do>
2022-06-06 15:26:06 -0400

    final touch

--- components/Filters/index.tsx
+++ components/Filters/index.tsx
@@ -34,7 +34,6 @@ export const Filters = ({
   }
 
   const handlePrice = (target: HTMLInputElement, name: string) => {
-    debugger
     if(filters.price === name) {
       (document.querySelectorAll('input[type=checkbox].price-filter') as NodeListOf<HTMLInputElement>).forEach(el => el.checked = false)
       setFilters({
